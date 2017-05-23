ce9dbebb0044c0b46959ca69a1abe559772e6580
Ivan R. Judson <irjudson@gmail.com>
2017-05-22 21:32:25 -0700

    Revert "Trying to make this work."
    This reverts commit b675142bead577001694251717cca0920e2a5333.

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,17 +3,17 @@ set -e
 
 if ! rake ar:version 2>/dev/null; then
     echo "Loading schema..."
-    rake ar:schema:create ar:schema:load > /dev/null
+    bundle exec rake ar:schema:load > /dev/null
 fi
 
-if [ "$(echo "$LOAD_CONGRESS" | tr '[:upper:]' '[:lower:]')" = "true" ]; then
+if [ "$RACK_ENV" != "test" -a "$(echo "$LOAD_CONGRESS" | tr '[:upper:]' '[:lower:]')" = "true" ]; then
     if [ ! -d /datasources/us_congress_members ]; then
         echo "Adding datasource..."
         ./phantom-dc datasource add --git-clone https://github.com/unitedstates/contact-congress.git us_congress /datasources/us_congress_members members/
     fi
 
     echo "Loading congress members..."
-    rake phantom-dc:update_git > /dev/null
+    bundle exec rake phantom-dc:update_git > /dev/null
 fi
 
 exec "$@"
